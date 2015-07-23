537a57b28f49ce7b82cb5e7d38ca2fb59c198ae6
ntakeshita <nolantakeshita@berkeley.edu>
2015-07-23 15:30:49 -0700

    website update

--- home.js
+++ home.js
@@ -2,26 +2,9 @@ var heightTop = null,
     menu = null,
     sticky = null;
 
-$( document ).ready(function() {
+$(document).ready(function() {
     menu = $('#menu_bar');
     heightTop = menu.offset().top;
-});
-
-/* Everytime window is scrolled, check slider function. */
-
-$(window).scroll(function() {
-    
-    if ($(window).scrollTop() > heightTop && !sticky) {
-        sticky = menu.clone();
-        sticky.prop('id', menu.prop('id') + '-clone');
-        sticky.insertBefore(menu);
-        menu.toggleClass('fixed');
-    } else if ($(window).scrollTop() < heightTop && sticky != null) {
-        sticky.remove();
-        sticky = null;
-        menu.toggleClass('fixed');
-    }
-    
 });
 
 /* Offset hashchange to account for menu size. */
