e5f535fa6270be05467a1cae0053efd39582240a
ntakeshita <nolantakeshita@berkeley.edu>
2015-07-24 04:21:46 -0700

    redid cover page

--- home.js
+++ home.js
@@ -7,19 +7,3 @@ $(document).ready(function() {
     heightTop = menu.offset().top;
 });
 
-/* Offset hashchange to account for menu size. */
-
-$(window).on("hashchange", function () {
-    window.scrollTo(window.scrollX, window.scrollY - menu.height());
-});
-
-/* Hover functionality for the board pictures. */
-
-$('.wrapper').hover(function() {
-    window.alert();
-    $(this, 'span').css('opacity', '1');
-}, function() {
-    $(this, 'span').css('opacity', '0');
-});
-
-
