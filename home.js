1632ab4b424b83444b487f8248e84dfd32e1a72d
ntakeshita <nolantakeshita@berkeley.edu>
2015-07-24 19:27:24 -0700

    updated board gallery

--- home.js
+++ home.js
@@ -1,9 +1,33 @@
+// Made by Nolan Takeshita in 2015.
+
 var heightTop = null,
     menu = null,
-    sticky = null;
+    sticky = null,
+    imgPath = null,
+    id = null;
+
 
 $(document).ready(function() {
     menu = $('#menu_bar');
     heightTop = menu.offset().top;
 });
 
+
+/* Apply a linear gradient and show text field on hover over. */
+$(function() {
+    $(".gallery_img").hover(
+        // Handler in for gallery photos.
+        function () {
+            $(this).find("span").css('visibility', 'visible');
+            id = '#'+ $(this).attr('id');
+            imgPath = $(id).css('background-image');
+            $(id).css('background-image', 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ' + imgPath);
+        },
+        // Handler out for gallery photos.
+        function () {
+            $(this).find("span").css('visibility', 'hidden');
+            $(id).css('background-image', imgPath);
+            id = imgPath = null;
+        }
+    );
+});
