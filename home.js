17523f6da722815f21fe43f62d69289520df8ccd
ntakeshita <nolantakeshita@berkeley.edu>
2015-10-07 15:26:46 -0700

    Removed Docs page

--- home.js
+++ home.js
@@ -33,11 +33,8 @@ $(function() {
         });
 });
 
-$(window).resize(function() {
-    $.stellar('refresh');
-    menuSizing();
-});
 
+// On clicking the icon show the menu
 $(function() {
     $('#menu_alt_icon').click(function() {
         $('#menu_alt').toggle();
@@ -52,26 +49,33 @@ $(function() {
     });
 });
 
+$(function() {
+    $('.menu_item').click(function() {
+        $('#menu_alt').toggle();
+    });
+});
+
+$(function() {
+    $(window).resize(function() {
+        menuSizing();
+    });
+});
+
 
 // Resizing function.
 function menuSizing() {
     var windowWidth = $(window).width(),
         menuHeight = $('#menu_bar').innerHeight();
     $('#menu_alt').css('margin-top', menuHeight + 'px');
-    if (windowWidth < 750) {
-        $('#bjacl').text("Berkeley JACL");
+    if (windowWidth < 1100) {
         $('#menu_alt_icon').css('display', 'inline');
         $('#menu').css('display', 'none');
-        $('.gallery_txt').css('font-size', '90%');
-    } else if (windowWidth < 1200) {
-        $('#bjacl').text("Berkeley JACL");
+        $('.gallery_txt').css('font-size', '80%');
+    } 
+    else {
+        $('#menu_alt').hide();
         $('#menu_alt_icon').css('display', 'none');
         $('#menu').css('display', 'inline');
         $('.gallery_txt').css('font-size', '110%');
-    } else {
-        $('#bjacl').text("Berekley Japanese American Citizens League");
-        $('#menu_alt_icon').css('display', 'none');
-        $('#menu').css('display', 'inline');
-        $('.gallery_txt').css('font-size', '120%');
     }
 }
